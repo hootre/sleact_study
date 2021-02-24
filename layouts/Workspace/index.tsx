717cf13fb869cfa5780955fa09f900c5f8a9323c
hootre <zxv7295@naver.com>
2021-02-24 17:27:15 +0900

    gravatar edit

--- layouts/Workspace/index.tsx
+++ layouts/Workspace/index.tsx
@@ -96,11 +96,11 @@ const Workspace: VFC = () => {
       <Header>
         <RightMenu>
           <span onClick={toggleUserProfile}>
-            <ProfileImg src={gravatar.url(userData.nickname, { s: '28px', d: 'retro' })} alt={userData.nickname} />
+            <ProfileImg src={gravatar.url(userData.email, { s: '28px', d: 'retro' })} alt={userData.email} />
             {showUserMenu && (
               <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onCloseModal={toggleUserProfile}>
                 <ProfileModal>
-                  <img src={gravatar.url(userData.nickname, { s: '36px', d: 'retro' })} alt={userData.nickname} />
+                  <img src={gravatar.url(userData.email, { s: '36px', d: 'retro' })} alt={userData.email} />
                   <div>
                     <span id="profile-name">{userData.nickname}</span>
                     <span id="profile-active">Active</span>
